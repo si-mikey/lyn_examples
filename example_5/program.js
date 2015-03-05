ae629349ce8a6cc002ce09a0168d9044c3b91919
= <mikeylopez@gmail.com>
2015-03-05 18:07:37 -0500

    LS filter program

---
+++ example_5/program.js
@@ -0,0 +1,15 @@
+fs  = require('fs');
+
+var path = process.argv[2];
+var ext  = process.argv[3];
+
+fs.readdir(path, function(err, list){
+  if(err) throw err;
+  list.forEach(function(item){
+   if(item.indexOf(".") != -1){
+    itemExt = item.substring(item.lastIndexOf(".") + 1, item.length) 
+    if(itemExt === ext)
+      console.log(item)
+   }
+  });
+});
